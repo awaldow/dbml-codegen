b5b2cc161b93eb513fbb7d14270e2b8da6fafefe
Addison Waldow <addison.waldow@phoenix.gov>
2021-01-19 15:03:24 -0700

    add refs list to Table

--- dbmlcodegen.parser/Models/Table.cs
+++ dbmlcodegen.parser/Models/Table.cs
@@ -8,12 +8,14 @@ namespace dbmlcodegen.parser.Models
         public string Alias { get; set; }
         public string Note { get; set; }
         public List<Column> Columns { get; set; }
-        public List<Index> Indexes {get;set;}
+        public List<Index> Indexes { get; set; }
+        public List<Ref> Refs { get; set; }
 
         public Table()
         {
             this.Columns = new List<Column>();
             this.Indexes = new List<Index>();
+            this.Refs = new List<Ref>();
         }
     }
 }
