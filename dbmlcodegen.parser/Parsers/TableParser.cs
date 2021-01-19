5e4cab8f0e48cc9ccad26bda441cf03b6a2f553b
Addison Waldow <addison.waldow@phoenix.gov>
2021-01-19 15:06:30 -0700

    parse table name and alias

--- dbmlcodegen.parser/Parsers/TableParser.cs
+++ dbmlcodegen.parser/Parsers/TableParser.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using dbmlcodegen.parser.Models;
 
 namespace dbmlcodegen.parser.Parsers
@@ -14,6 +15,34 @@ namespace dbmlcodegen.parser.Parsers
         public Table Generate()
         {
             var ret = new Table();
+            var line = "";
+            using (StringReader reader = new StringReader(table))
+            {
+                do
+                {
+                    line = reader.ReadLine();
+                    if (line.IndexOf("{") != -1) // First line, might have table name and alias, also note
+                    {
+                        var split = line.Split(" ");
+                        if (split[1] != "{")
+                        {
+                            ret.Name = split[1];
+                        }
+                        if (split.Length == 5)
+                        {
+                            ret.Alias = split[3];
+                        }
+                    }
+                    else if (line.IndexOf("{") == -1 && line.IndexOf("}") != 0) // Intermediate row
+                    {
+                        var split = line.Split(":");
+                        var field = split[0].Trim();
+                        var value = split[1].Trim().Replace("\'", string.Empty);
+                    }
+                }
+                while (line.IndexOf("}") == -1 && line != null);
+            }
+
             return ret;
         }
     }
